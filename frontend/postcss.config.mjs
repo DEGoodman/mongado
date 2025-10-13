35e8b0e3f7a751bc2d20aef7f8a53dafa58f33c5
Erik Goodman <erik-gh@unoseeme.com>
2025-10-13 15:20:15 -0500

    downgrade Tailwind while keeping other major package upgrades

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,9 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    'tailwindcss': {},
+    'autoprefixer': {},
   },
-};
-
-export default config;
+}
